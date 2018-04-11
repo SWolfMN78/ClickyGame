1fbfa603dbdc045bc4c5e242140c994c129b81e8
Wolfknightx <Wolfknightx@Gmail.com>
2018-04-10 19:08:09 -0500

    starting into project

---
+++ clickyme/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+
+import Panel from "./components/Panel";
+
+const App = () => (
+  <div className="container">
+    <Navbar />
+    
+    <Panel />
+  </div>
+);
+
+export default App;
---
+++ clickyme/src/components/Bootstrap/Navbar.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Navbar = () => (
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" 
+    type="button" data-toggle="collapse" 
+    data-target="#navbarColor02" aria-controls="navbarColor02" 
+    aria-expanded="false" aria-label="Toggle navigation" 
+    style="">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+  <div class="collapse navbar-collapse" id="navbarColor02">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About</a>
+      </li>
+    </ul>
+    <input class="form-control" id="disabledInput iCurrentScore" type="text" placeholder="00" disabled=""/>
+    <input class="form-control" id="disabledInput iTopScore" type="text" placeholder="00" disabled=""/>    
+  </div>
+</nav>
+
+);
+
+export default Navbar;
